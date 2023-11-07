697cb6f2c8f1c61017d8322fa0ff09274be4bcf8
Mahdi_moahamadiha <m.mohamadiha81@gmail.com>
2023-11-07 13:33:52 +0330

    Add 'Single Responsibility Principle' Files

---
+++ SRP/With_SRP/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace With_SRP
+{
+	public class Logger : ILogger
+	{
+		public Logger()
+		{
+			// here we need to write the Code for initialization 
+			// that is Creating the Log file with necesssary details
+		}
+		public void Info(string info)
+		{
+			// here we need to write the Code for info information into the ErrorLog text file
+		}
+		public void Debug(string info)
+		{
+			// here we need to write the Code for Debug information into the ErrorLog text file
+		}
+		public void Error(string message, Exception ex)
+		{
+			// here we need to write the Code for Error information into the ErrorLog text file
+		}
+	}
+}
