a9d02e230821cb90ac78c73082b7933b1cba2bf7
Mahdi_moahamadiha <m.mohamadiha81@gmail.com>
2023-11-11 17:57:06 +0330

    Add 'Liskov Substitution Principle' Files

---
+++ LSP/With_LSP/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace With_LSP
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			IFruit fruit = new Orange();
+			Console.WriteLine($"Color of Orange: {fruit.GetColor()}");
+			fruit = new Apple();
+			Console.WriteLine($"Color of Apple: {fruit.GetColor()}");
+			Console.ReadKey();
+		}
+	}
+}
